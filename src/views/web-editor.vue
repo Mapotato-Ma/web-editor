4729463d2866dc1b0ba161575b970f6e2a793cd5
Mapotato <pf-ma@qq.com>
2023-08-22 08:05:46 +0800

    feat:删除自动生成文件

---
+++ src/views/web-editor.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="web-editor">编辑器</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped>
+.web-editor {
+  display: grid;
+  place-content: center;
+}
+</style>
